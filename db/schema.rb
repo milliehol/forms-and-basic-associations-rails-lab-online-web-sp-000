171e2fe7f19a636cdec1541c368479fa811e5a1d
Carla Holcomb <carla.programmer@gmail.com>
2020-04-19 18:24:14 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114013240) do
+ActiveRecord::Schema.define(version: 20160114013250) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160114013240) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "song_id"
+    t.index ["song_id"], name: "index_notes_on_song_id"
   end
 
   create_table "songs", force: :cascade do |t|
